76e3ee131aecb2ab5346e7e3100cf0815665b511
TurkishP <jk_9397@hotmail.com>
2018-04-25 12:15:28 +0900

    fixed sednToServer method where there was data loss and unreliability in communication. Too much println and dealy seems to have been the problem? Quite weird because giving dealy between POST requests were what fixed the problem of 'too rapid of post requests'

--- nodeMCU_Codes_Combined.ino
+++ nodeMCU_Codes_Combined.ino
@@ -15,7 +15,7 @@ int fileCount = 1;
 int fileSubCount = 1;
 double amp = 0;
 int stringConcat = 1;
-int fftCount = 0;
+//int fftCount = 0;
 String line;
 String stringToSend;
 int numOfDataSent=0;
@@ -40,7 +40,6 @@ uint16_t packetSize;    // expected DMP packet size   (default is 42 bytes)
 uint16_t fifoCount;     // count of all bytes currently in FIFO
 uint8_t fifoBuffer[64]; // FIFO storage buffer
 
-
 // orientation/motion vars
 Quaternion q;           // [w, x, y, z]         quaternion container
 VectorInt16 aa;         // [x, y, z]            accel sensor measurements
@@ -50,20 +49,17 @@ VectorFloat gravity;    // [x, y, z]            gravity vector
 float euler[3];         // [psi, theta, phi]    Euler angle container
 float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
 
-
 //INTERRUPT DETECTION ROUTINE
 volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
 void dmpDataReady() {
   mpuInterrupt = true;
 }
-
 //needed for samping
 // Idling
 //unsigned long firstMillis = 0 ;
 unsigned long firstMicros = 0 ;
 unsigned long thisMicros = 0 ;
 unsigned long idx = 1 ;
-
 
 //********************************   SD card headers + variables    ***********************************
 #include <SD.h>
@@ -83,8 +79,8 @@ float ax, ay, az;
 #include <SPI.h>
 String buff;
 
-char *ssid = "genesis";
-char *password = "12345678";
+char *ssid = "BCI Lab";
+char *password = "bcilab317a";
 //char *content ="{\"n\":\"daehoandmyeonghu\"}";
 
 //**************************   FFT headers + variables + definitions   ************************************
@@ -106,7 +102,6 @@ int count = 0;
 
 File dataFile1, dataFile2, dataFile3, results;
 
-
 // ======================================================================================================
 //                          /$$$$$$  /$$$$$$$$ /$$$$$$$$ /$$   /$$ /$$$$$$$
 //                         /$$__  $$| $$_____/|__  $$__/| $$  | $$| $$__  $$
@@ -119,8 +114,8 @@ File dataFile1, dataFile2, dataFile3, results;
 // ======================================================================================================
 void setup() {
   Serial.begin(115200);
-
-   mpu.setDMPEnabled(false);
+  pinMode(button, INPUT);
+//   mpu.setDMPEnabled(false);
 ////   //get time stamp
 if(control == 1){
   
@@ -187,8 +182,7 @@ if(control == 1){
     results.close();
 
 }
-      
-
+ 
   //*********************************************    MPU6050 Setup   *****************************************
 
   // join I2C bus (I2Cdev library doesn't do this automatically)
@@ -210,11 +204,9 @@ if(control == 1){
   Serial.println(F("Testing device connections..."));
   Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
 
-
   // load and configure the DMP
   Serial.println(F("Initializing DMP..."));
   devStatus = mpu.dmpInitialize();
-
 
   //sensor offsets
   mpu.setXGyroOffset(-1249);
@@ -223,7 +215,6 @@ if(control == 1){
   mpu.setXAccelOffset(179); // 1688 factory default for my test chip
   mpu.setYAccelOffset(-67); // 1688 factory default for my test chip
   mpu.setZAccelOffset(0); //
-
 
   // make sure it worked (returns 0 if so)
   if (devStatus == 0) {
@@ -261,36 +252,33 @@ if(control == 1){
     Serial.println("SD card opened successfully");
   }
   firstMicros = micros() ;
+ mpu.setDMPEnabled(false);
 
   //*********************************************    FFT  Setup   *****************************************
 // For recording data
 control =3;
-if(control ==1){
-    for(int x =1; x<4; x++){
-     SD.remove(String(x) + ".TXT");
-     Serial.println("Erased "+ String(x) + ".TXT");
-     }
-     
-    dataFile3 = SD.open("3.TXT", FILE_WRITE);
-    dataFile2 = SD.open("2.TXT", FILE_WRITE);
-    dataFile1 = SD.open("1.TXT", FILE_WRITE);
-}
-
-
-// For FFT
-if(control ==2){
-    dataFile1 = SD.open("1.txt");
-    dataFile2 = SD.open("2.txt");
-    dataFile3 = SD.open("3.txt");
-}
+//    if(control ==1){
+//        for(int x =1; x<4; x++){
+//         SD.remove(String(x) + ".TXT");
+//         Serial.println("Erased "+ String(x) + ".TXT");
+//         }
+//         
+//        dataFile3 = SD.open("3.TXT", FILE_WRITE);
+//        dataFile2 = SD.open("2.TXT", FILE_WRITE);
+//        dataFile1 = SD.open("1.TXT", FILE_WRITE);
+//    }
+//
+//
+//    // For FFT
+//    if(control ==2){
+//        dataFile1 = SD.open("1.txt");
+//        dataFile2 = SD.open("2.txt");
+//        dataFile3 = SD.open("3.txt");
+//    }
   sampling_period_us = round(1000000 * (1.0 / SAMPLING_FREQUENCY));
 }
 
-
-
-
 // ======================================================================================================
-
 //       /$$      /$$  /$$$$$$  /$$$$$$ /$$   /$$       /$$        /$$$$$$   /$$$$$$  /$$$$$$$
 //      | $$$    /$$$ /$$__  $$|_  $$_/| $$$ | $$      | $$       /$$__  $$ /$$__  $$| $$__  $$
 //      | $$$$  /$$$$| $$  \ $$  | $$  | $$$$| $$      | $$      | $$  \ $$| $$  \ $$| $$  \ $$
@@ -299,50 +287,18 @@ if(control ==2){
 //      | $$\  $ | $$| $$  | $$  | $$  | $$\  $$$      | $$      | $$  | $$| $$  | $$| $$
 //      | $$ \/  | $$| $$  | $$ /$$$$$$| $$ \  $$      | $$$$$$$$|  $$$$$$/|  $$$$$$/| $$
 //      |__/     |__/|__/  |__/|______/|__/  \__/      |________/ \______/  \______/ |__/
-
 // ======================================================================================================
 void loop() {
-//  // read the pushbutton input pin:
-//  buttonState = digitalRead(buttonPin);
-//
-//  // compare the buttonState to its previous state
-//  if (buttonState != lastButtonState) {
-//    // if the state has changed, increment the counter
-//    if (buttonState == HIGH) {
-//      // if the current state is HIGH then the button went from off to on:
-//      buttonPushCounter++;
-//      Serial.println("on");
-//      Serial.print("number of button pushes: ");
-//      Serial.println(buttonPushCounter);
-//    } else {
-//      // if the current state is LOW then the button went from on to off:
-//      Serial.println("off");
-//    }
-//    // Delay a little bit to avoid bouncing
-//    delay(30);
-//  }
-//  // save the current state as the last state, for next time through the loop
-//  lastButtonState = buttonState;
 
   if (control == 1) {
-    mpu.setDMPEnabled(true); 
     recordData();
-    
   } else if (control == 2) {
     doFFT();
-//    Serial.println("FFT");
   } else if (control == 3) {
-
     mpu.setDMPEnabled(false);
     sendtoServer();
   }
-
-
 }
-
-
-
-
 
 //                               /$$$$$$$$ /$$$$$$$$ /$$$$$$$$
 //                              | $$_____/| $$_____/|__  $$__/
@@ -400,16 +356,6 @@ void doFFT() {
     double meanis = FFT.meanis(vReal, amp);
     double SDis = FFT.SDis(vReal, amp);
 
-    //      for(int i=0; i<(SAMPLES/2); i++)
-    //      {
-    //          /*View all these three lines in serial terminal to see which frequencies has which amplitudes*/
-    //
-    //          //Serial.print((i * 1.0 * SAMPLING_FREQUENCY) / SAMPLES, 1);
-    //          //Serial.print(" ");
-    //
-    //          Serial.println(vReal[i], 1);    //View only this line in serial plotter to visualize the bins
-    //      }
-
     //Measuring how much time FFT takes.
     unsigned long CurrentTime = millis();
     unsigned long ElapsedTime = CurrentTime - StartTime;
@@ -462,7 +408,7 @@ void doFFT() {
   }//xyz loop end
 
   //Write results to file.
-  results = SD.open("result2.txt", FILE_WRITE);
+  results = SD.open("result.txt", FILE_WRITE);
 
   if(stringConcat%7==0){
       dataString = ("{\"x_hz\":\"" + String(x_hz) + "\",\"x_amp\":\"" + String(x_amp) + "\",\"x_use\":\"" + String(x_use) + "\",\"y_hz\":\"" + String(y_hz) + "\",\"y_amp\":\"" + String(y_amp) + "\",\"y_use\":\"" + String(y_use) + "\",\"z_hz\":\"" + String(z_hz) + "\",\"z_amp\":\"" + String(z_amp) + "\",\"z_use\":\"" + String(z_use) + "\"},");
@@ -473,14 +419,10 @@ void doFFT() {
       stringConcat= stringConcat +1;
       results.print(dataString);
   }
-  fftCount = fftCount+1;
+//  fftCount = fftCount+1;
   Serial.println(dataString);
   results.close();
 }
-
-
-
-
 
 //         /$$$$$$$  /$$$$$$$$  /$$$$$$   /$$$$$$  /$$$$$$$  /$$$$$$$        /$$$$$$$   /$$$$$$  /$$$$$$$$ /$$$$$$
 //        | $$__  $$| $$_____/ /$$__  $$ /$$__  $$| $$__  $$| $$__  $$      | $$__  $$ /$$__  $$|__  $$__//$$__  $$
@@ -567,10 +509,6 @@ varForDataCount = varForDataCount+1;
 }
 
 
-
-
-
-
 //                /$$$$$$  /$$$$$$$$ /$$$$$$$  /$$    /$$ /$$$$$$$$ /$$$$$$$
 //               /$$__  $$| $$_____/| $$__  $$| $$   | $$| $$_____/| $$__  $$
 //              | $$  \__/| $$      | $$  \ $$| $$   | $$| $$      | $$  \ $$
@@ -580,7 +518,7 @@ varForDataCount = varForDataCount+1;
 //              |  $$$$$$/| $$$$$$$$| $$  | $$   \  $/   | $$$$$$$$| $$  | $$
 //               \______/ |________/|__/  |__/    \_/    |________/|__/  |__/
 void sendtoServer() {
- fftCount =0;
+// fftCount =0;
   WiFi.mode(WIFI_STA);
   WiFi.disconnect();
 
@@ -603,10 +541,10 @@ void sendtoServer() {
   char *host = "capstone.cafe24app.com";
   int port = 80;
 
-//  if (!httpClient.connect(host, port)) {
-//    Serial.println("[WiFi] connection failed...");
-//    return;
-//  }
+  if (!httpClient.connect(host, port)) {
+    Serial.println("[WiFi] connection failed...");
+    return;
+  }
 
 //  Serial.println("[WiFi] request from the client was handled...");
   // http의 get 메세지 사용 웹페이지 요청
@@ -614,69 +552,69 @@ void sendtoServer() {
 
   results = SD.open("result.txt");
 
-  if (results) {
-   
-
-    //read first line which contains time information FFT data
-    stringToSend = results.readStringUntil('\n');
-    String timeAndUserNo = stringToSend;
-    String start = timeAndUserNo.substring(0,timeAndUserNo.indexOf("Sent")+7);
-    String finish = timeAndUserNo.substring(timeAndUserNo.indexOf("Sent")+8);
-    
-     // read from the file until there's nothing else in it:
-    while(results.available()){
-          if (!httpClient.connect(host, port)) {
-              Serial.println("[WiFi] connection failed...");
-              return;
-          }
-      //each line has 7 minutes worth of data.
-      //so here in the loop we are sending 5 lines making it 35 min worth of data
-      for(int x= 0; x<5; x++){
-        buff = results.readStringUntil('\n');
-        Serial.println("read Number " + String(x) + " and buff : " + buff);
-        stringToSend = stringToSend + buff;
-      }
-
-       //after concatenating the data, get rid of the hanging comma at the end
-       //and properly end the string by adding ]}.
-        stringToSend.remove(stringToSend.lastIndexOf(",")); 
-        stringToSend = stringToSend.substring(0, stringToSend.length()) + "]}";
-        
-        //if it is not the first line, we need to concatenate {"data":[ to fit data transfer protocol
-        if(stringToSend.substring(2,6) != "time"){
-           stringToSend = start + String(numOfDataSent) + finish + stringToSend;
+        if (results) {
+          //read first line which contains time information FFT data
+          stringToSend = results.readStringUntil('\n');
+          String timeAndUserNo = stringToSend;
+          String start = timeAndUserNo.substring(0,timeAndUserNo.indexOf("Sent")+7);
+          String finish = timeAndUserNo.substring(timeAndUserNo.indexOf("Sent")+8);
+          Serial.println(start);
+          Serial.println(finish);
+          
+           // read from the file until there's nothing else in it:
+                    while(results.available()){
+                          if (!httpClient.connect(host, port)) {
+                              Serial.println("[WiFi] connection failed...");
+                              return;
+                          }
+                      //each line has 7 minutes worth of data.
+                      //so here in the loop we are sending 5 lines making it 35 min worth of data
+                      for(int x= 0; x<5; x++){
+                        buff = results.readStringUntil('\n');
+//                        Serial.println("read Number " + String(x) + " and buff : " + buff);
+                        stringToSend = stringToSend + buff;
+                      }
+                
+                       //after concatenating the data, get rid of the hanging comma at the end
+                       //and properly end the string by adding ]}.
+                        stringToSend.remove(stringToSend.lastIndexOf(","));
+                        stringToSend = stringToSend.substring(0, stringToSend.length()) + "]}";
+                        
+                        //if it is not the first line, we need to concatenate {"data":[ to fit data transfer protocol
+                        buff = stringToSend;
+                        stringToSend = start + String(numOfDataSent);
+                        stringToSend = stringToSend + finish + buff;
+                        
+                        httpClient.println("POST /v/data HTTP/1.1");
+                        httpClient.println("Host: capstone.cafe24app.com");
+                        httpClient.println("Connection: close");
+                        httpClient.println("Content-Type: application/json");
+                        httpClient.print("Content-Length: ");
+                        httpClient.println(stringToSend.length());
+                        httpClient.println();
+                        httpClient.println(stringToSend);
+                        Serial.println(stringToSend); 
+                        stringToSend = "";
+                        numOfDataSent = numOfDataSent + 35;
+                        
+                //      웹 서버로부터 수신된 데이터를 줄 단위로 출력
+                      while (httpClient.available()) {
+                        String line = httpClient.readStringUntil('\r');
+                        Serial.print(line);
+                      }
+                
+                //      delay(100);
+                      yield();
+                      httpClient.stop();
+                    }
+                    
+          // close the file:
+          results.close();
+          numOfDataSent = 0;
+      
+        } else {
+          // if the file didn't open, print an error:
+          Serial.println("Error Opening File");
+      
         }
-        
-        httpClient.println("POST /v/data HTTP/1.1");
-        httpClient.println("Host: capstone.cafe24app.com");
-        httpClient.println("Connection: close");
-        httpClient.println("Content-Type: application/json");
-        httpClient.print("Content-Length: ");
-        httpClient.println(stringToSend.length());
-        httpClient.println();
-        httpClient.println(stringToSend);
-        Serial.println(stringToSend); 
-        stringToSend = "";
-        numOfDataSent = numOfDataSent +35;
-        
-      //웹 서버로부터 수신된 데이터를 줄 단위로 출력
-//      while (httpClient.available()) {
-//        String line = httpClient.readStringUntil('\r');
-//        Serial.print(line);
-//      }
-
-      delay(70);
-      yield();
-           httpClient.stop();
-    }
-      httpClient.stop();
-    // close the file:
-    results.close();
-    numOfDataSent = 0;
-
-  } else {
-    // if the file didn't open, print an error:
-    Serial.println("Error Opening File");
-
-  }
 }
